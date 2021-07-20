d827501e47a90c79c8a8dd6db09dcd39c5222dd7
Kelvin Morrison <kelvinninja1@gmail.com>
2021-07-20 18:19:50 +0000

    Added: Contact list Display

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from '../contact';
 
+import { CONTACTS } from '../mock-contacts';
+
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
+  contacts = CONTACTS;
   contact: Contact = {
     id: 1,
     name: 'Developer',
@@ -15,9 +18,12 @@ export class ContactsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
   }
 
+  selectedContact?: Contact;
+  onSelect(contact: Contact): void {
+    this.selectedContact = contact;
+  }
 }
 
 
---
+++ src/app/mock-contacts.ts
@@ -0,0 +1,14 @@
+import { Contact } from './contact';
+
+export const CONTACTS: Contact[] = [
+  { id: 11, name: 'Dr Nice', phone: '(+233) 55 183 8987' },
+  { id: 12, name: 'Narco', phone: '(+233) 55 183 8987' },
+  { id: 13, name: 'Bombasto', phone: '(+233) 55 183 8987' },
+  { id: 14, name: 'Celeritas', phone: '(+233) 55 183 8987' },
+  { id: 15, name: 'Magneta', phone: '(+233) 55 183 8987' },
+  { id: 16, name: 'RubberMan', phone: '(+233) 55 183 8987' },
+  { id: 17, name: 'Dynama', phone: '(+233) 55 183 8987' },
+  { id: 18, name: 'Dr IQ', phone: '(+233) 55 183 8987' },
+  { id: 19, name: 'Magma', phone: '(+233) 55 183 8987' },
+  { id: 20, name: 'Tornado', phone: '(+233) 55 183 8987' }
+];
